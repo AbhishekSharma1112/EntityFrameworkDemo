f078c07973c30d7e1be4172bc1edc5dcd92e4139
Abhishek <as111298@gmail.com>
2023-10-08 21:15:51 +0530

    Add project files.

---
+++ EntityFrameworkDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EntityFrameworkDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ EntityFrameworkDemo/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkDemo.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RollNumber { get; set; }
+        public string Class { get; set; }
+    }
+}
