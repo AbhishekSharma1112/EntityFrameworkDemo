c2b982cc7abb99c37e9def480089d20ec3a593d1
Abhishek <as111298@gmail.com>
2023-10-09 11:45:12 +0530

    Task-2

---
+++ EntityFrameworkDemo/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using EntityFrameworkDemo.Data;
+using EntityFrameworkDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetAll()
+        {
+            var students = await _context.Students.ToListAsync();
+            return Ok(students);
+        }
+
+    }
+}
